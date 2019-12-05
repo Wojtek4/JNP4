4e8e86fbd7542d9886173de09de623dd9b10337a
Wojciech Szymanski <wojciech.szymanski@students.mimuw.edu.pl>
2019-12-05 23:19:49 +0100

    Trzeba dodac funkcje zwracajace funkcje

--- fibin.h
+++ fibin.h
@@ -15,6 +15,8 @@ constexpr uint64_t Var(const char * s) {
 	for (auto it = s; *it != '\0'; it++) {
 		result *= 256;
 		result += *it;
+		if (*it <= 'Z' && *it >= 'A')
+			result += 'a' - 'A';
 	}
 	return result;
 }
@@ -23,15 +25,18 @@ template <uint64_t Name1, typename Value1, typename Tail>
 struct ContextType{
 	template <typename ValueType, uint64_t name, typename FullContext>
 	static constexpr ValueType eval() {
-		if (Name1 == name)
+		if constexpr (name == Name1)
 			return Value1::template eval<ValueType, FullContext>();
-		return Tail::template eval<ValueType, name, FullContext>();
+		else
+			return Tail::template eval<ValueType, name, FullContext>();
 	}
+
 	template <typename ValueType, uint64_t name, typename Argument, typename FullContext>
 	static constexpr ValueType evalFunction() {
-		if (Name1 == name)
+		if constexpr (name == Name1)
 			return Value1::template evalFunction<ValueType, Argument, FullContext>();
-		return Tail::template evalFunction<ValueType, name, Argument, FullContext>();
+		else
+			return Tail::template evalFunction<ValueType, name, Argument, FullContext>();
 	}
 };
 
@@ -56,7 +61,7 @@ template <typename Left, typename Right>
 struct Eq {
 	template <typename ValueType, typename Context>
 	static constexpr bool eval() {
-		static_assert(std::is_same<ValueType, bool>::value, "Binary method with nonbinary return type");
+		static_assert(std::is_same<ValueType, bool>::value, "EQ returns True/False, not a number");
 		return Left::template eval<ValueType, Context>() == Right::template eval<ValueType, Context>();
 	}
 };
@@ -65,17 +70,18 @@ template <auto n>
 struct Fib {
 	template <typename ValueType, typename Context = EmptyContext>
 	static constexpr ValueType eval() {
-		static_assert(!std::is_same<ValueType, bool>::value, "Nonbinary method with binary return type");
+		static_assert(!std::is_same<ValueType, bool>::value, "Number is not a True/False value");
 		static_assert(n >= 0);
-		if (n < 2)
+		if constexpr (n < 2)
 			return n;
-		return Fib<n - 1>::template eval<ValueType>() + Fib<n - 2>::template eval<ValueType>();
+		else
+			return Fib<n - 1>::template eval<ValueType>() + Fib<n - 2>::template eval<ValueType>();
 	}
 };
 
 template <typename T>
 struct Lit{
-	static_assert(!std::is_same<T,T>::value);
+	static_assert(!std::is_same<T,T>::value, "Wrong Literal");
 };
 
 template <auto n>
@@ -88,18 +94,18 @@ struct Lit<Fib<n>>{
 
 template <>
 struct Lit<False> {
-	template <typename ValueType>
+	template <typename ValueType, typename Context = EmptyContext>
 	static constexpr ValueType eval() {
-		static_assert(std::is_same<ValueType, bool>::value, "Binary method with nonbool return type");
+		static_assert(std::is_same<ValueType, bool>::value, "False is not a number");
 		return false;
 	}
 };
 
 template <>
 struct Lit<True> {
-	template <typename ValueType>
+	template <typename ValueType, typename Context = EmptyContext>
 	static constexpr ValueType eval() {
-		static_assert(std::is_same<ValueType, bool>::value, "Binary method with nonbool return type");
+		static_assert(std::is_same<ValueType, bool>::value, "True is not a number");
 		return true;
 	}
 };
@@ -108,14 +114,14 @@ template <typename Condition, typename Then, typename Else>
 struct If {
 	template <typename ValueType, typename Context>
 	static constexpr ValueType eval() {
-		if (Condition::template eval<bool, Context>())
+		if constexpr (Condition::template eval<bool, Context>())
 			return Then::template eval<ValueType, Context>();
 		else
 			return Else::template eval<ValueType, Context>();
 	}
 	template <typename ValueType, typename Argument, typename Context>
 	static constexpr ValueType evalFunction() {
-		if (Condition::template eval<bool, Context>())
+		if constexpr (Condition::template eval<bool, Context>())
 			return Then::template evalFunction<ValueType, Argument, Context>();
 		else
 			return Else::template evalFunction<ValueType, Argument, Context>();
@@ -135,6 +141,22 @@ struct Sum<E1, E2> {
 	template <typename ValueType, typename Context>
 	static constexpr ValueType eval() {
 		return E1::template eval<ValueType, Context>() + E2::template eval<ValueType, Context>();
+	}
+};
+
+template<typename Arg>
+struct Inc10{
+	template <typename ValueType, typename Context>
+	static constexpr ValueType eval() {
+		return Sum<Lit<Fib<10>>, Arg>:: template eval<ValueType, Context>();
+	}
+};
+
+template<typename Arg>
+struct Inc1{
+	template <typename ValueType, typename Context>
+	static constexpr ValueType eval() {
+		return Sum<Lit<Fib<1>>, Arg>:: template eval<ValueType, Context>();
 	}
 };
 
